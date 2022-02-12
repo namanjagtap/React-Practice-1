636f3edc26a74a55e5cb7041b5a851521da58f54
Naman Jagtap <namanjagtap05@gmail.com>
2022-02-12 16:20:30 +0530

    Changes Commited

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import React, {Component} from "react"
-import EmplDetail from "./emplDetails"
-import emplData from "./employees"
+import React, {useState, useEffect} from "react"
 
-class App extends Component{
-  constructor(){
-    super()
-    this.state={
-      empl: emplData
-    }
-  }
+const useFetch = url =>{
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-  render(){
-    const emplId = this.state.empl.map(detail => 
-    <EmplDetail 
-    key={detail.employeeCode}
-    detail={detail}
-    />)
+    useEffect(async () =>{
+        const response = await fetch(url);
+        const data = await response.json();
+        const [item] = data.results;
+        setData(item);
+        setLoading(false);
+    }, []); 
+
+    return {data, loading};
+}
+
+export default () => {
+    const [count, setCount] = useState(0);
+    const {data, loading} = useFetch("https://api.randomuser.me/");
 
     return(
-      <div>
-        {emplId}
-      </div>
+        <div>
+            <p>Your count is {count}</p>
+            <button onClick = {() => setCount(prevCount => prevCount+1)}>Click Here</button>
+            {loading ? <div>loading...</div> : <div>{data.name.first}</div>}
+        </div>
     )
-  }
 }
-export default App;
